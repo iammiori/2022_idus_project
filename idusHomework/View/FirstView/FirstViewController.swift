34f08f6a5d43a594dae5c5336eb1960d82190a68
Mi Yeon Lee <immioriii@gmail.com>
2022-08-12 16:27:37 +0900

    TA2022-6 feat: 검색뷰 vc에 넣고, 화면이동 구현

---
+++ idusHomework/View/FirstView/FirstViewController.swift
@@ -0,0 +1,60 @@
+//
+//  FirstViewController.swift
+//  idusHomework
+//
+//  Created by miori Lee on 2022/08/12.
+//
+
+import UIKit
+
+class FirstViewController: BaseViewController {
+
+    let idusLogoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "logo")
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    let searchBar = FirstSearchBarView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNaviBackButton()
+        setAttribute()
+        setLayout()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        searchBar.addGestureRecognizer(tapGesture)
+    }
+    
+    private func setNaviBackButton() {//arrow.backward
+        let backBarButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backBarButton
+    }
+    private func setAttribute() {
+        self.title = "iOS 이미연 홈워크"
+    }
+    private func setLayout() {
+        [idusLogoImageView,searchBar].forEach {
+            self.view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        NSLayoutConstraint.activate([
+            idusLogoImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 12),
+            idusLogoImageView.centerYAnchor.constraint(equalTo: searchBar.centerYAnchor),
+            idusLogoImageView.heightAnchor.constraint(equalTo: searchBar.heightAnchor),
+            
+            searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -12),
+            searchBar.leadingAnchor.constraint(equalTo: idusLogoImageView.trailingAnchor, constant: 8)
+        ])
+    }
+}
+
+extension FirstViewController {
+   // 3. this method is called when a tap is recognized
+   @objc func handleTap(sender: UITapGestureRecognizer) {
+       let nextVC = SearchViewController()
+       self.navigationController?.pushViewController(nextVC, animated: true)
+   }
+}
