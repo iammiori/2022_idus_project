343c47a15df6dcef9cd7fc7148ff9e7b50013d36
Mi Yeon Lee <immioriii@gmail.com>
2022-08-13 19:28:57 +0900

    TA2022-19 feat:  URLComponents 구현으로 코드 수정

--- idusHomework/Service/NetworkService.swift
+++ idusHomework/Service/NetworkService.swift
@@ -10,22 +10,19 @@ import Combine
 
 final class NetworkService {
     let session: URLSession
-
-
+    
     init(configuration: URLSessionConfiguration) {
-        session = URLSession(configuration: configuration)
+    session = URLSession(configuration: configuration)
     }
     
-    func fetchInfo(appID: Int) -> AnyPublisher<AppStoreResponse, Error> {
+    func fetchInfo(url: URL) -> AnyPublisher<AppStoreResponse, Error> {
         //872469884
-        let url = URL(string: "\(URLConstant.appstoreSarchQueryID)\(appID)")!
 
         let publisher = session
             .dataTaskPublisher(for: url)
             .tryMap { result -> Data in
                 guard let httpResponse = result.response as? HTTPURLResponse,
                       (200..<300).contains(httpResponse.statusCode) else {
-                    
                     let response = result.response as? HTTPURLResponse
                     let statusCode = response?.statusCode ?? -1
                     throw NetworkError.responseError(statusCode: statusCode)
