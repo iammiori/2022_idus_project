7fb2b6ffa5d98549e52e0286a5429b834cc2c068
Mi Yeon Lee <immioriii@gmail.com>
2022-08-14 19:57:29 +0900

    TA2022-22 feat: searchVC의 뷰모델 구현

---
+++ idusHomework/VIewModel/SearchView/SearchViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  SearchViewModel.swift
+//  idusHomework
+//
+//  Created by miori Lee on 2022/08/14.
+//
+
+import Foundation
+import Combine
+
+enum SearchState {
+    case able
+    case unable
+}
+
+final class SearchViewModel {
+    
+    let networkService: NetworkService
+    
+    init(network: NetworkService = NetworkService(configuration: .default), searchedModel: AppStoreResponse?) {
+        self.networkService = network
+        self.searchedModel = CurrentValueSubject(searchedModel)
+    }
+    
+    var subscriptions = Set<AnyCancellable>()
+    let searchedModel: CurrentValueSubject<AppStoreResponse?, Never>
+   
+    var buttonHidden: Bool {
+        if searchedModel.value?.resultCount ?? 0 > 0 {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    var name: String {
+        if searchedModel.value?.resultCount ?? 0 > 0 {
+            return searchedModel.value?.results[0].sellerName ?? ""
+        } else if searchedModel.value?.resultCount ?? 0 == 0 {
+            return "검색결과가 없습니다"
+        } else {
+            return ""
+        }
+    }
+    var logoImgURL: String {
+        if searchedModel.value?.resultCount ?? 0 > 0 {
+            return searchedModel.value?.results[0].artworkUrl60 ?? ""
+        } else {
+            return ""
+        }
+    }
+    
+    func search(appID: String) {
+        let resource = NetworkResource<AppStoreResponse>(scheme: "http",
+                                                         host: "itunes.apple.com",
+                                                         path: "/lookup",
+                                                         params: ["id":appID],
+                                                         header: [:],
+                                                         httpMethod: "GET")
+        networkService.fetchInfo(resource)
+            .receive(on: RunLoop.main)
+            .sink { completion in
+                print("completion: \(completion)")
+                //에러처리
+                switch completion {
+                case .failure(let error):
+                    self.searchedModel.send(nil)
+                case .finished:
+                    break
+                }
+            } receiveValue: { appInfo in
+                self.searchedModel.send(appInfo)
+            }.store(in: &subscriptions)
+    }
+}
