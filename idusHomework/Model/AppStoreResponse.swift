04965e4eaf5fcff4fe9753d8eb6873e67e235596
Mi Yeon Lee <immioriii@gmail.com>
2022-08-15 20:06:56 +0900

    TA2022-35 feat: 추가적으로 받아올 모델 구현

--- idusHomework/Model/AppStoreResponse.swift
+++ idusHomework/Model/AppStoreResponse.swift
@@ -12,6 +12,7 @@ struct AppStoreResponse: Codable {
     let results: [AppStoreResult]
 }
 struct AppStoreResult: Codable {
+    let trackName: String
     let artworkUrl60: String
     let sellerName: String
     let description: String
