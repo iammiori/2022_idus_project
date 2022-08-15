6ca13cdc0a7402f7094bf3247696944f50bd7ca1
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-08-15 20:03:44 +0900

    TA2022-34 feat: 모델 받아와서, 뷰에 뿌릴수 있게 변환하는 뷰모델 구현 (#17)

--- idusHomework/VIewModel/DetailView/DetailViewModel.swift
+++ idusHomework/VIewModel/DetailView/DetailViewModel.swift
@@ -14,4 +14,24 @@ final class DetailViewModel {
     init(appInfo: AppStoreResponse) {
         self.appInfo = appInfo
     }
+    
+    var appName: String {
+        return appInfo?.results[0].trackName ?? ""
+    }
+    var corpName: String {
+        return appInfo?.results[0].sellerName ?? ""
+    }
+    var appLogoURL: String {
+        return appInfo?.results[0].artworkUrl60 ?? ""
+    }
+    var description: String {
+        return appInfo?.results[0].description ?? ""
+    }
+    var previewURLArr: [String] {
+        return appInfo?.results[0].screenshotUrls ?? []
+    }
+    
+    var numberOfpreviewArr: Int {
+        return previewURLArr.count
+    }
 }
