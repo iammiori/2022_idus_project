727091a39b40262db3ef8df3a57767ec5430285c
Mi Yeon Lee <immioriii@gmail.com>
2022-08-17 13:54:14 +0900

    TA2022-41 feat: 이미지 캐싱 구현

--- idusHomework/Configuration/Extension/UIImageView.swift
+++ idusHomework/Configuration/Extension/UIImageView.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 
+struct ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+}
 extension UIImageView {
     func loadImagefromURL(stringURL: String) {
+        let cacheKey = NSString(string: stringURL)
+        if let cachedImg = ImageCacheManager.shared.object(forKey: cacheKey) {
+            self.image = cachedImg
+            return
+        }
         if stringURL.count > 0 {
             let url = URL(string: stringURL)
             DispatchQueue.global().async  { [weak self] in
                 if let data = try? Data(contentsOf: url!) {
                     if let image = UIImage(data: data) {
                         DispatchQueue.main.async {
+                            ImageCacheManager.shared.setObject(image, forKey: cacheKey)
                             self?.image = image
                         }
                     }
