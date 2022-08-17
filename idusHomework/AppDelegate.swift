935cf68e84b2d216b72c1b544ddd06a7105d992d
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-08-17 13:55:25 +0900

    TA2022-41 feat: 이미지 캐싱 구현(메모리) (#25)
    * TA2022-41 modify: 네트워크 검사 함수 이전
    
    * TA2022-41 feat: 이미지 캐싱 구현

--- idusHomework/AppDelegate.swift
+++ idusHomework/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        CheckNetwork.shared.startMonitoring()
         return true
     }
 
@@ -27,13 +26,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        print("dd")
-        CheckNetwork.shared.stopMonitoring()
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
 }
 
--- idusHomework/SceneDelegate.swift
+++ idusHomework/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: scene)
-        //UINavigationController(rootViewController: mainViewController)
+        CheckNetwork.shared.startMonitoring()
         window?.rootViewController = UINavigationController(rootViewController: FirstViewController())
         window?.makeKeyAndVisible()
     }
