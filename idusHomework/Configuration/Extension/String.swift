577d76f8235b442277f0ee1684d8e25dbe3fb74f
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-08-17 15:12:30 +0900

    TA2022-50 : 한국어 다국어 설정후, 장르 영한변경 (#30)
    * TA2022-50 feat: 한국어 다국어 설정
    
    * TA2022-50 feat: 다국어 쉽게 받아올수 있게 구현

---
+++ idusHomework/Configuration/Extension/String.swift
@@ -0,0 +1,16 @@
+//
+//  String.swift
+//  idusHomework
+//
+//  Created by miori Lee on 2022/08/17.
+//
+
+import Foundation
+
+extension String {
+    
+    func localized(comment: String = "") -> String {
+        return NSLocalizedString(self, comment: comment)
+    }
+    
+}
