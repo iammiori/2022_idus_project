b6ae63007ff1097bce1ac6932a4bb8f282810475
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-08-17 16:19:04 +0900

    TA2022-52 test: 디테일뷰모델 테스트 (성공) (#33)

--- idusHomeworkTests/DetailViewModelTests.swift
+++ idusHomeworkTests/DetailViewModelTests.swift
@@ -25,9 +25,9 @@ class DetailViewModelTests: XCTestCase {
         model = nil
     }
 
-    func test_model의_데이터가_의도된대로_viewModel프로퍼티에_담기는지() {
+    func test_model의_데이터가_정상적일때_의도된대로_viewModel프로퍼티에_담기는지() {
         //given
-        let model = AppStoreResponse(resultCount: 1, results: [AppStoreResult(trackName: "testTrackName", artworkUrl60: "testArtworkUrl60", sellerName: "testSellerName", description: "testDescription", screenshotUrls: ["test1","test2"])])
+        let model = AppStoreResponse(resultCount: 1, results: [AppStoreResult(trackName: "testTrackName", artworkUrl60: "testArtworkUrl60", sellerName: "testSellerName", description: "testDescription", screenshotUrls: ["test1","test2","test3"], releaseNotes: "testRelease", version: "testVersion", currentVersionReleaseDate: "2022-08-16T02:05:27Z", fileSizeBytes: "12345", primaryGenreName: "Social Networking")])
 
         let viewModel = DetailViewModel(appInfo: model)
         //when
@@ -36,17 +36,36 @@ class DetailViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.corpName, "testSellerName")
         XCTAssertEqual(viewModel.appLogoURL, "testArtworkUrl60")
         XCTAssertEqual(viewModel.description, "testDescription")
-        XCTAssertEqual(viewModel.previewURLArr, ["test1","test2"])
+        XCTAssertEqual(viewModel.previewURLArr, ["test1","test2","test3"])
+        XCTAssertEqual(viewModel.releaseNote, "testRelease")
+        XCTAssertEqual(viewModel.currentVersionReleaseDate, "2022-08-16")
+        XCTAssertEqual(viewModel.version,"testVersion")
+        XCTAssertEqual(viewModel.numberOfpreviewArr, 3)
+        XCTAssertEqual(viewModel.korGenre, "소셜 네트워킹")
     }
     
-    func test_previewURLArr의_길이를_제대로_반환하는지() {
-        //given
-        let model = AppStoreResponse(resultCount: 1, results: [AppStoreResult(trackName: "testTrackName", artworkUrl60: "testArtworkUrl60", sellerName: "testSellerName", description: "testDescription", screenshotUrls: ["test1","test2"])])
-
-        let viewModel = DetailViewModel(appInfo: model)
-        //when
-        //then
-        XCTAssertEqual(viewModel.numberOfpreviewArr, 2)
+    func test_sliceDateFormat_호출시_yyyyMMdd_포맷을_반환하는지() {
+        let str = "2022-08-16T02:05:27Z"
+        let result = sut.sliceDateFormat(str: str)
+        XCTAssertEqual(result, "2022-08-16")
+    }
+    
+    func test_checkNumberOfLine_호출시_lineCount가_4미만일때_true를_반환하는지() {
+        let lineCount: Int = 3
+        let reuslt = sut.checkNumberOfLine(lineCount)
+        XCTAssertEqual(reuslt, true)
+    }
+    
+    func test_checkNumberOfLine_호출시_lineCount가_4이상일때_false를_반환하는지() {
+        let lineCount: Int = 20
+        let reuslt = sut.checkNumberOfLine(lineCount)
+        XCTAssertEqual(reuslt, false)
+    }
+    
+    func test_convertByteToMB_호출시_제대로_계산하는지() {
+        let byteNum: Int64 = 1048576
+        let mbNum = sut.convertByteToMB(byteNum)
+        XCTAssertEqual(mbNum, "1MB")
     }
 
 }
